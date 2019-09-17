365dacbd7f37cd895f0e79d3185e9d77f84d40bc
mimuls <mimul@bodiesandsouls.com>
2019-09-17 15:56:48 +0900

    설정 정보 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `미물의 기술 로그`,
+    description: `mimul's tech logs.`,
+    author: `@mimul`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+	{
+		resolve: "gatsby-source-filesystem",
+		options: {
+			path: `${__dirname}/src/pages`,
+			name: "pages"
+		}
+	},
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -38,7 +38,7 @@ const Layout = ({ children }) => {
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="http://mimul.com">mimul</a>
         </footer>
       </div>
     </>
